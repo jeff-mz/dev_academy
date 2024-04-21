6f4a5a57f135e63907d7ebe856376cf00480c228
Jafar Mirzapoor <152167471+jeff-mz@users.noreply.github.com>
2024-04-21 09:31:18 +0330

    Update main with my solution.js

--- src/04-reverseString/main.js
+++ src/04-reverseString/main.js
@@ -6,7 +6,14 @@ function reverseStringV2(str) {
   // Using arrays:
 }
 
+// Jeff mz :
+const reverseStringJ = (str) => {
+  console.log(str.toLowerCase().split("").reverse().join(""));
+};
 module.exports = {
   reverseStringV1,
   reverseStringV2,
+  reverseStringJ
 };
+
+
